3ba1a6a008f3c03d802d5dc465a4e772bd87b2f7
Moritz Schmötzer <moritz.schmoetzer@uni-ulm.de>
2025-07-17 10:08:37 +0200

    Fixed the reset button

--- UnitConverter/src/main/java/moritz/schmoetzer/UnitConverter/controller/ConverterController.java
+++ UnitConverter/src/main/java/moritz/schmoetzer/UnitConverter/controller/ConverterController.java
@@ -10,11 +10,10 @@ import moritz.schmoetzer.UnitConverter.services.ConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@SessionAttributes("converter")
 public class ConverterController {
 
     private final ConverterService converterService;
@@ -27,24 +26,32 @@ public class ConverterController {
     public String getUnitConverter(@RequestParam(required = false) String quantity, Model model){
         Converter converter;
 
-        switch ((quantity != null ? quantity : "length")){
-            case "length":
-                converter = new Converter(Quantity.LENGTH, LengthUnits.METER, LengthUnits.KILOMETER, null, null);
-                break;
-            case "weight":
-                converter = new Converter(Quantity.WEIGHT, WeightUnits.GRAM, WeightUnits.KILOGRAM, null, null);
-                break;
-            case "temperature":
-                converter = new Converter(Quantity.TEMPERATURE, TemperatureUnits.CELSIUS, TemperatureUnits.FAHRENHEIT, null, null);
-                break;
-            default:
-                converter = new Converter(Quantity.LENGTH, LengthUnits.METER, LengthUnits.METER, null, null);
-                break;
+        if (!model.containsAttribute("converter")){ // If no converter was instantiated before e.g. opening the site for the first time
+            converter = new Converter(Quantity.LENGTH, LengthUnits.METER, LengthUnits.KILOMETER, null, null);
+            model.addAttribute(converter);
+        } else{
+            converter = (Converter) model.getAttribute("converter");
+            if (quantity != null){
+                if (!converter.getQuantity().name().toLowerCase().equals(quantity)){ // If the user is switching to a different converter --> providing a new converter with sample values
+                    switch (quantity){
+                        case "length":
+                            converter = new Converter(Quantity.LENGTH, LengthUnits.METER, LengthUnits.KILOMETER, null, null);
+                            break;
+                        case "weight":
+                            converter = new Converter(Quantity.WEIGHT, WeightUnits.GRAM, WeightUnits.KILOGRAM, null, null);
+                            break;
+                        case "temperature":
+                            converter = new Converter(Quantity.TEMPERATURE, TemperatureUnits.CELSIUS, TemperatureUnits.FAHRENHEIT, null, null);
+                            break;
+                    }
+                }
+            } else{ // Getting redirected after resetting the converter --> clearing the initial- and target-value.
+                converter.setInitialValue(null);
+                converter.setTargetValue(null);
+            }
         }
 
-        // TODO: Fix reset button --> display selected quantity and units when resetting
-
-        model.addAttribute(converter);
+        model.addAttribute("converter", converter);
         model.addAttribute("measurementUnits", getUnitsForQuantity(converter.getQuantity()));
 
         return "unitConverter.html";
@@ -56,7 +63,6 @@ public class ConverterController {
         converter.setTargetValue(convertResult);
 
         model.addAttribute("measurementUnits", getUnitsForQuantity(converter.getQuantity()));
-        model.addAttribute(converter);
 
         return "unitConverter.html";
     }
